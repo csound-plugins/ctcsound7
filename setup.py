97f8dc2e648a7128f4f20e9f716effd707bf72e8
gesellkammer <eduardo.moguillansky@gmail.com>
2023-04-03 16:29:13 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+classifiers = """
+Topic :: Multimedia :: Sound/Audio
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+"""
+
+setup(name='ctcsound7',
+      version='0.1.0',
+      url='https://github.com/csound-plugins/ctcsound7',
+      description='Python bindings to the Csound API using ctypes', 
+      classifiers=filter(None, classifiers.split('\n')),
+      py_modules=['ctcsound7'],
+      install_requires=[
+        'numpy>=1.16'
+      ]
+)
+
+
