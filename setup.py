b65cf41521c8a5fe12e2aafd67447d1ed99a3ef9
gesellkammer <eduardo.moguillansky@gmail.com>
2023-04-03 16:31:22 +0200

    readme in rst

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Programming Language :: Python :: 3
 """
 
 setup(name='ctcsound7',
-      version='0.1.0',
+      version='0.2.0',
       url='https://github.com/csound-plugins/ctcsound7',
       description='Python bindings to the Csound API using ctypes', 
       classifiers=filter(None, classifiers.split('\n')),
