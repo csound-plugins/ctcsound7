f37d535e16629e8abcf495a7899218300dc986e8
gesellkammer <eduardo.moguillansky@gmail.com>
2024-12-12 18:06:20 +0100

    version

--- examples/csound7-perfthread-callback.py
+++ examples/csound7-perfthread-callback.py
@@ -1,6 +1,7 @@
 import ctcsound7
 import time
 import ctypes
+import queue
 
 assert ctcsound7.VERSION >= 7000
 
@@ -12,29 +13,42 @@ sr = 48000
 0dbfs = 1
 nchnls = 2
 
+chnset 1000, "kfreq"
+
 instr 1
-  prints "init instr 1"
-  if metro(10) == 1 then
-    println "perf instr 1"
-  endif
+  kfreq = chnget("kfreq")
+  a0 oscili 0.1, kfreq
+  outch 1, a0
+endin
+
+instr 2
+  a0 = pinker() * linseg:a(0, 0.005, 1, 0.01, 0)
+  outch 2, a0*0.5
 endin
 ''')
 
+q = queue.Queue()
 
 def proc(data):
-    print("----", data)
-    
+    if q.empty():
+        return
+    job = q.get(block=False)
+    if job:
+        job()
+
+
 
 cs.start()
 thread = cs.performanceThread()
-print(thread.processCallback())
-data = ctcsound7.CsoundParams()
-param = ctypes.c_void_p()
-thread.setProcessCallback(proc, param)
+thread.setProcessCallback(proc, ctypes.c_void_p())
 thread.play()
 
 thread.scoreEvent(0, "i", [1, 0, 10])
+thread.scoreEvent(0, "i", [2, 1, 1])
+
+time.sleep(1)
+q.put(lambda: cs.setControlChannel("kfreq", 500))
+time.sleep(3)
 
 
-time.sleep(2)
 thread.stop()
